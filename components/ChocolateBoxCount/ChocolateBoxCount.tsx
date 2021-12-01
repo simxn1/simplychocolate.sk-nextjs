f6fbf3562aff30e78cae7545c941a7d836c362d0
Šimon Munka <mcmunka@gmail.com>
2021-12-01 22:44:59 +0100

    Added ChocolateBoxCount;

---
+++ components/ChocolateBoxCount/ChocolateBoxCount.tsx
@@ -0,0 +1,69 @@
+import { FC } from "react";
+import styles from "styles/modules/ChocolateBoxes.module.css";
+import { useCartContext } from "context/CartContext";
+import { chocolateBoxes } from "data/chocolate-boxes";
+import {
+  CartContextLocalStorageKeys,
+  setAndSaveToLocalStorage,
+} from "lib/utils";
+
+interface Props {}
+
+export const ChocolateBoxCount: FC<Props> = ({}: Props) => {
+  const { chocolateBoxesQuantity, setChocolateBoxesQuantity } =
+    useCartContext();
+
+  const handleChangeQuantity = (
+    thisChocolateBoxIndex: number,
+    amountToChangeBy: number
+  ) => {
+    chocolateBoxesQuantity &&
+      setAndSaveToLocalStorage(
+        () => {
+          let newChocolateBoxesQuantity = [...chocolateBoxesQuantity];
+          if (
+            newChocolateBoxesQuantity[thisChocolateBoxIndex] > 0 ||
+            amountToChangeBy > 0
+          ) {
+            newChocolateBoxesQuantity[thisChocolateBoxIndex] =
+              newChocolateBoxesQuantity[thisChocolateBoxIndex] +
+              amountToChangeBy;
+          }
+          return newChocolateBoxesQuantity;
+        },
+        setChocolateBoxesQuantity,
+        CartContextLocalStorageKeys.ChocolateBoxesQuantity
+      );
+  };
+
+  return (
+    <div className={styles.count}>
+      <div className="product-quantities">
+        <strong>množstvo</strong>
+        {chocolateBoxesQuantity?.map((quantity, index) => {
+          const thisProduct = chocolateBoxes[index];
+          const imgName = thisProduct.name.replace(/\s+/g, "-");
+
+          if (!thisProduct.isSoldOut)
+            return (
+              <div className={styles.quantity}>
+                <img
+                  src={`/img/chocolate-boxes/mutual/${imgName}.webp`}
+                  alt=""
+                />
+                <div className={styles.counter} key={index}>
+                  <button onClick={() => handleChangeQuantity(index, -1)}>
+                    -
+                  </button>
+                  <span>{quantity}</span>
+                  <button onClick={() => handleChangeQuantity(index, 1)}>
+                    +
+                  </button>
+                </div>
+              </div>
+            );
+        })}
+      </div>
+    </div>
+  );
+};
---
+++ data/chocolate-boxes.ts
@@ -0,0 +1,78 @@
+export interface IChocolateBox {
+  name: string;
+  firstLine: string;
+  secondLine: string;
+  color: string;
+  secondaryColor: string;
+  price: number;
+  pieces: number;
+  nutritionData: string[];
+  nutritionDesc: string;
+  isSoldOut?: boolean;
+  isHalfDiscounted?: boolean;
+}
+
+export const chocolateBoxes: IChocolateBox[] = [
+  {
+    name: "sharing is caring",
+    firstLine: "sharing ",
+    secondLine: "is caring",
+    color: "#2a5284",
+    secondaryColor: "#071f3e",
+    price: 1200,
+    pieces: 12,
+    nutritionData: [
+      "2123 kJ/507 kCal",
+      "32 g",
+      "19 g",
+      "46 g",
+      "44 g",
+      "8,2 g",
+      "0,24 g",
+    ],
+    nutritionDesc:
+      "Kakaová hmota, cukor, kakaové maslo, celé MLIEKO v prášku, MANDLE, sušený kokos, karamelové chrumky, SÓJOVÁ bielkovina, emulgátor SÓJOVÝ lecitín, prírodná vanilková príchuť, sladké drievko, maliny (sušené mrazom), nerafinovaná morská soľ, soľ, citrónový olej, kávové zrná.",
+    isSoldOut: true,
+  },
+  {
+    name: "instead of flowers",
+    firstLine: "instead ",
+    secondLine: "of flowers",
+    color: "#ef8599",
+    secondaryColor: "#803547",
+    price: 1200,
+    pieces: 12,
+    nutritionData: [
+      "2123 kJ/507 kCal",
+      "32 g",
+      "19 g",
+      "46 g",
+      "44 g",
+      "8,2 g",
+      "0,24 g",
+    ],
+    nutritionDesc:
+      "Kakaová hmota, cukor, kakaové maslo, celé MLIEKO v prášku, MANDLE, sušený kokos, karamelové chrumky, SÓJOVÁ bielkovina, emulgátor SÓJOVÝ lecitín, prírodná vanilková príchuť, sladké drievko, maliny (sušené mrazom), nerafinovaná morská soľ, soľ, citrónový olej, kávové zrná.",
+  },
+  {
+    name: "yes you can buy love",
+    firstLine: "yes you ",
+    secondLine: "can buy love",
+    color: "#82bbbd",
+    secondaryColor: "#536d71",
+    price: 2000,
+    pieces: 24,
+    nutritionData: [
+      "2311 kJ/552 kCal",
+      "36 g",
+      "22 g",
+      "46 g",
+      "44 g",
+      "8,4 g",
+      "0,26 g",
+    ],
+    nutritionDesc:
+      "Kakaová hmota, cukor, kakaové maslo, celé MLIEKO v prášku, MANDLE, sušený kokos, karamelové chrumky, SÓJOVÁ bielkovina, emulgátor SÓJOVÝ lecitín, prírodná vanilková príchuť, sladké drievko, maliny (sušené mrazom), nerafinovaná morská soľ, soľ, citrónový olej, kávové zrná.",
+    isSoldOut: true,
+  },
+];
