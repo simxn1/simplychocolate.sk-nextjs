fae3beb3dad028b1f3d2cda76a618996c5a6480c
Šimon Munka <mcmunka@gmail.com>
2021-12-02 14:55:41 +0100

    Added ChocolateBoxCart;

---
+++ components/ChocolateBoxCart/ChocolateBoxCart.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import styles from "styles/modules/ChocolateBoxes.module.css";
+import { ChocolateBoxCount } from "../ChocolateBoxCount";
+
+interface Props {
+  toggleIsCartOpen: () => void;
+  handleContinue: () => void;
+  handleCheckout: () => void;
+}
+
+export const ChocolateBoxCart: FC<Props> = ({
+  toggleIsCartOpen,
+  handleContinue,
+  handleCheckout,
+}: Props) => {
+  return (
+    <div className={styles.cart}>
+      <i className="fas fa-times" onClick={toggleIsCartOpen} />
+      <h1 className="heading">máš chuť na čokolásku?</h1>
+      <p>objednaj si Simply Chocolate už dnes!</p>
+      <ChocolateBoxCount />
+      <button onClick={handleContinue}>mám chuť aj na tyčinky</button>
+      <button onClick={handleCheckout}>mám všetko a chcem platiť</button>
+    </div>
+  );
+};
