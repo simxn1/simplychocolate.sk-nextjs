1ce55cb0a8f42b3b4fae9dfc05170e0c71cf7e52
Šimon Munka <mcmunka@gmail.com>
2021-12-04 13:31:43 +0100

    Updated CartContext, additions in globalTypes;

--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -8,16 +8,25 @@ import {
   useState,
 } from "react";
 import { chocolateBoxes } from "data/chocolate-boxes";
-
-const CartContext = createContext<ICartContext>({
-  chocolateBoxesQuantity: null,
-  setChocolateBoxesQuantity: null,
-});
+import { chocolateBars } from "data/chocolate-bars";
 
 interface ICartContext {
   chocolateBoxesQuantity: number[] | null;
   setChocolateBoxesQuantity: Dispatch<SetStateAction<number[]>> | null;
+  chocolateBarsQuantity: number[] | null;
+  setChocolateBarsQuantity: Dispatch<SetStateAction<number[]>> | null;
+  totalChocolateBarsQuanity: number | null;
+  setTotalChocolateBarsQuanity: Dispatch<SetStateAction<number>> | null;
 }
+
+const CartContext = createContext<ICartContext>({
+  chocolateBoxesQuantity: null,
+  setChocolateBoxesQuantity: null,
+  chocolateBarsQuantity: null,
+  setChocolateBarsQuantity: null,
+  totalChocolateBarsQuanity: null,
+  setTotalChocolateBarsQuanity: null,
+});
 
 interface Props {
   children: ReactNode;
@@ -28,9 +37,20 @@ export const CartContextWrapper: FC<Props> = ({ children }: Props) => {
     number[]
   >(chocolateBoxes.map(() => 0));
 
+  const [chocolateBarsQuantity, setChocolateBarsQuantity] = useState<number[]>(
+    chocolateBars.map(() => 0)
+  );
+
+  const [totalChocolateBarsQuanity, setTotalChocolateBarsQuanity] =
+    useState<number>(0);
+
   const shared: ICartContext = {
     chocolateBoxesQuantity,
     setChocolateBoxesQuantity,
+    chocolateBarsQuantity,
+    setChocolateBarsQuantity,
+    totalChocolateBarsQuanity,
+    setTotalChocolateBarsQuanity,
   };
 
   return <CartContext.Provider value={shared}>{children}</CartContext.Provider>;
--- lib/globalTypes.ts
+++ lib/globalTypes.ts
@@ -1,3 +1,6 @@
+import { IChocolateBox } from "data/chocolate-boxes";
+import { IChocolateBar } from "data/chocolate-bars";
+
 export interface Product {
   name: string;
   color: string;
@@ -7,4 +10,11 @@ export interface Product {
   nutritionDesc: string;
   isSoldOut?: boolean;
   isHalfDiscounted?: boolean;
+}
+
+export type ProductType = IChocolateBox | IChocolateBar;
+
+export enum SectionId {
+  ChocolateBoxes = "chocolate-boxes",
+  ChocolateBars = "chocolate-bars",
 }
