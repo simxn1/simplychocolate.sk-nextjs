04ef36635d1515d2c9667bb228cf8085cacee342
Šimon Munka <mcmunka@gmail.com>
2021-12-06 00:16:09 +0100

    Added mix page;

---
+++ pages/mix.tsx
@@ -0,0 +1,201 @@
+import type { NextPage } from "next";
+import styles from "styles/modules/Mix.module.css";
+import { boxSizes, chocolateBars, Size } from "data/chocolate-bars";
+import { chocolateBoxes } from "data/chocolate-boxes";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { useCartContext } from "context/CartContext";
+import { setAndSaveToLocalStorage, stringifyPrice } from "lib/utils";
+import { CartContextLocalStorageKeys } from "lib/globalTypes";
+
+const Mix: NextPage = () => {
+  const router = useRouter();
+  const {
+    chocolateBarsQuantity,
+    selectedChocolateBarsBoxSize,
+    totalPrice,
+    setChocolateBarsQuantity,
+    setSelectedChocolateBarsBoxSize,
+  } = useCartContext();
+
+  const [isInfoVisible, setIsInfoVisible] = useState<boolean>(false);
+  const [errSizeNotSelected, setErrSizeNotSelected] = useState<boolean>(false);
+  const [errQuantity, setErrQuantity] = useState<boolean>(false);
+
+  const selectBoxSize = (size: Size) => {
+    setAndSaveToLocalStorage<Size | null>(
+      size,
+      setSelectedChocolateBarsBoxSize,
+      CartContextLocalStorageKeys.SelectedChocolateBarsBoxSize
+    );
+    setErrSizeNotSelected(false);
+  };
+
+  const handleGoBack = () => {
+    router.back();
+  };
+
+  const handleContinue = () => {
+    if (currentAvailableBarsQuantity !== totalChocolateBarsQuantity) {
+      setErrQuantity(true);
+    } else router.push("/cart-check");
+  };
+
+  const handleChocolateBox = () => {
+    if (currentAvailableBarsQuantity !== totalChocolateBarsQuantity) {
+      setErrQuantity(true);
+    } else router.push("/chocolate-box");
+  };
+
+  const changeThisBarQuantity = (
+    barIndex: number,
+    amountToChangeBy: number
+  ) => {
+    if (!selectedChocolateBarsBoxSize) {
+      setErrSizeNotSelected(true);
+    }
+
+    if (
+      selectedChocolateBarsBoxSize &&
+      currentAvailableBarsQuantity &&
+      currentAvailableBarsQuantity >=
+        totalChocolateBarsQuantity + amountToChangeBy
+    ) {
+      setErrQuantity(false);
+
+      setAndSaveToLocalStorage<number[]>(
+        (function () {
+          const newBarsQuantity = [...(chocolateBarsQuantity ?? [])];
+
+          if (newBarsQuantity[barIndex] + amountToChangeBy >= 0) {
+            newBarsQuantity[barIndex] =
+              newBarsQuantity[barIndex] + amountToChangeBy;
+          }
+
+          return newBarsQuantity;
+        })(),
+        setChocolateBarsQuantity,
+        CartContextLocalStorageKeys.ChocolateBarsQuantity
+      );
+    }
+  };
+
+  const totalChocolateBarsQuantity = chocolateBarsQuantity
+    ? chocolateBarsQuantity.reduce((a, b) => a + b, 0)
+    : 0;
+
+  const currentAvailableBarsQuantity = boxSizes?.find(
+    (boxSize) => boxSize.size === selectedChocolateBarsBoxSize
+  )?.barCount;
+
+  useEffect(() => {
+    if (selectedChocolateBarsBoxSize) setIsInfoVisible(true);
+  }, [selectedChocolateBarsBoxSize]);
+
+  return (
+    <div className={styles.mix}>
+      <span onClick={handleGoBack} className={styles.back}>
+        <i className="fas fa-long-arrow-alt-left" />
+      </span>
+      <h1>
+        Mám chuť na
+        <br />
+        na poriadnu čokoládu!
+      </h1>
+      <h2>veľkosť boxu</h2>
+      <ul className={styles.boxSizeSelection}>
+        {boxSizes.map((boxSize) => (
+          <li key={boxSize.size}>
+            <button
+              className={
+                selectedChocolateBarsBoxSize === boxSize.size
+                  ? styles.active
+                  : ""
+              }
+              onClick={() => selectBoxSize(boxSize.size)}
+            >
+              {boxSize.size}
+            </button>
+          </li>
+        ))}
+      </ul>
+      <h2>tyčinky v boxe</h2>
+      <ul className={styles.barSelection}>
+        {chocolateBars.map((bar, index) => {
+          return (
+            !bar.isSoldOut && (
+              <li className={bar.isHalfDiscounted ? styles.isDiscounted : ""}>
+                <img
+                  src={`/img/mix/bar/${bar.name
+                    .replace(" ", "")
+                    .toLowerCase()}.png`}
+                  alt={""}
+                />
+                {bar.name}
+                <div className={styles.barQuantity}>
+                  <button onClick={() => changeThisBarQuantity(index, -1)}>
+                    -
+                  </button>
+                  <input
+                    value={
+                      chocolateBarsQuantity?.length &&
+                      chocolateBarsQuantity[index]
+                    }
+                    type="text"
+                    readOnly
+                  />
+                  <button onClick={() => changeThisBarQuantity(index, 1)}>
+                    +
+                  </button>
+                </div>
+              </li>
+            )
+          );
+        })}
+      </ul>
+      {isInfoVisible && (
+        <h3>
+          Zostávajúci počet tyčiniek v boxe:
+          <strong>
+            {currentAvailableBarsQuantity
+              ? currentAvailableBarsQuantity - totalChocolateBarsQuantity
+              : 0}
+          </strong>
+        </h3>
+      )}
+      {chocolateBars.some((bar) => bar.isHalfDiscounted) && (
+        <p className={styles.isDiscounted}>
+          Končí mi spotreba, máš ma so zľavou 50%
+        </p>
+      )}
+      {isInfoVisible && (
+        <h4>
+          Cena celkom:
+          <strong className={styles.price}>
+            {totalPrice ? stringifyPrice(totalPrice) : 0}€
+          </strong>
+        </h4>
+      )}
+      {errQuantity && (
+        <span className={styles.error}>
+          Uveďte správny počet tyčiniek podľa veľkosti boxu.
+        </span>
+      )}
+      {errSizeNotSelected && (
+        <span className={styles.error}>Vyberte si veľkosť boxu.</span>
+      )}
+      {isInfoVisible && (
+        <div className={styles.continue}>
+          {!chocolateBoxes.every((box) => box.isSoldOut) && (
+            <button onClick={handleChocolateBox}>
+              Mám chuť aj na bonboniéru!
+            </button>
+          )}
+          <button onClick={handleContinue}>Chcem platiť!</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Mix;
